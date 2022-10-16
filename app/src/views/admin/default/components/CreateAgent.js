844284509d5ea2e220078a13297961f840c7e017
TarasPriadka <taras.priadka@gmail.com>
2022-10-16 09:55:30 -0700

    Handle more agent create fields.

--- app/src/views/admin/default/components/CreateAgent.js
+++ app/src/views/admin/default/components/CreateAgent.js
@@ -11,6 +11,7 @@ import {
     ModalCloseButton,
     useDisclosure,
     FormControl,
+    Select,
     FormLabel,
     FormErrorMessage,
     FormHelperText,
@@ -23,23 +24,32 @@ import {useSetRecoilState} from "recoil";
 import {agentsAtom} from "views/admin/atoms";
 
 import Nft1 from "assets/img/nfts/Nft1.png";
+import Nft2 from "assets/img/nfts/Nft2.png";
+import Nft3 from "assets/img/nfts/Nft3.png";
+import Nft4 from "assets/img/nfts/Nft4.png";
+import Nft5 from "assets/img/nfts/Nft5.png";
+import Nft6 from "assets/img/nfts/Nft6.png";
+
+const images = [Nft1, Nft2, Nft3, Nft4,Nft5, Nft6];
 
 export default function CreateAgent(props) {
     const {isOpen, onOpen, onClose} = useDisclosure();
     const setAgents = useSetRecoilState(agentsAtom);
 
     let [agentName, setAgentName] = useState("");
+    let [agentDescription, setAgentDescription] = useState("");
+    let [agentImage, setAgentImage] = useState(1);
 
     const onSubmit = (e) => {
         e.preventDefault();
         console.log(agentName);
-
+        const randomImage =  images[Math.floor(Math.random() * images.length)];
         const newAgent = {
             name: agentName,
             status: "Training",
             usage : "0",
-            description: "",
-            image: Nft1,
+            description: agentDescription,
+            image: randomImage,
         };
 
         setAgents((oldAgents) => [...oldAgents, newAgent]);
@@ -60,6 +70,8 @@ export default function CreateAgent(props) {
                         <FormControl>
                             <FormLabel>Agent Name</FormLabel>
                             <Input type='name' value={agentName} onChange={(e)=>{setAgentName(e.target.value)}} />
+                            <FormLabel>Agent Description</FormLabel>
+                            <Input type='name' value={agentDescription} onChange={(e)=>{setAgentDescription(e.target.value)}} />
                             <FormLabel>Training Files</FormLabel>
                             <Dropzone/>
                         </FormControl>
@@ -68,7 +80,7 @@ export default function CreateAgent(props) {
                         <Button colorScheme='blue' mr={3} onClick={onSubmit}>
                         Submit
                         </Button>
-                        <Button variant='danger'>Cancel</Button>
+                        <Button variant='danger' onClick={onClose}>Cancel</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
