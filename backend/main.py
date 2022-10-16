e3b7b30b15d5dc474f0dc4f153a3e052915dc811
Val Rotan <val.rotan@gmail.com>
2022-10-16 02:29:32 -0700

    endpoints

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,11 @@ class ExplainedResponse(BaseModel):
     sources: list[str]
 
 
+@app("/")
+def home():
+    return "Hello World!"
+
+
 @app.post("/query")
 def query(query: Query):
     response, sources = co.query(query.query)
@@ -25,3 +30,23 @@ def query(query: Query):
         response=response,
         sources=sources
     )
+
+
+@app.post("/create_agent")
+def new_agent():
+    raise NotImplementedError
+
+
+@app.get("/list_agents")
+def agents():
+    raise NotImplementedError
+
+
+@app.post("/update_agent")
+def update_agent():
+    raise NotImplementedError
+
+
+@app.post("/delete_agent")
+def delete_agent():
+    raise NotImplementedError
