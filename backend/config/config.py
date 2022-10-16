d3ab8156051a89b9a1ae2982590ad791a044c67a
Val Rotan <val.rotan@gmail.com>
2022-10-16 12:29:33 -0700

    switch to 161

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,20 +1,20 @@
 import uuid
 
 
-# data_dir = 'data/'
-# txt_dir = 'data/txt/'
-# links_path = 'data/161_links.txt'
-# csv_path = 'data/161.csv'
-data_dir = 'data_calhacks/'
-txt_dir = 'data_calhacks/txt/'
-links_path = 'data_calhacks/links.txt'
-csv_path = 'data_calhacks/data.csv'
+data_dir = 'data/'
+txt_dir = 'data/txt/'
+links_path = 'data/161_links.txt'
+csv_path = 'data/161.csv'
+# data_dir = 'data_calhacks/'
+# txt_dir = 'data_calhacks/txt/'
+# links_path = 'data_calhacks/links.txt'
+# csv_path = 'data_calhacks/data.csv'
 
 # cohere
 api_key = 'sMGG1mMkfOqx7zoZSPgXXkLmfWgsbSfIW1XMtR9j'
 embed_model = 'medium'
-# generate_model = 'e4759a69-7173-4458-bc78-594ae7aa1992-ft' # 161
-generate_model = '226da206-2cf7-43a8-8ee4-75b0262ec510-ft' # sponsors
+generate_model = 'e4759a69-7173-4458-bc78-594ae7aa1992-ft' # 161
+# generate_model = '226da206-2cf7-43a8-8ee4-75b0262ec510-ft' # sponsors
 embedding_size = 2048
 
 # other
